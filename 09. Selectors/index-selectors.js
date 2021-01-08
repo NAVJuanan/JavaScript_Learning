977329faf7dd666694fc8dbf0583145fbd026a45
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-01-08 10:02:36 +0100

    Program 09 ->Selectors

--- 09. Selectors/index-selectors.js
+++ 09. Selectors/index-selectors.js
@@ -13,3 +13,14 @@ console.log(nodeItem1);
 
 // write html code
 nodeDiv.innerHTML = "<h2>Write div with 'innerHTML'</h2>";
+
+// change style
+function changeColor() {
+    let nodeHi = document.querySelector("#hi");
+
+    if (nodeHi.style.color === "black") {
+        nodeHi.style.color = "red";
+    } else {
+        nodeHi.style.color = "black";
+    }
+}
