07c73342c644672356f0ed926087cd357e695a2b
Juanan MS Dynamics 365 Business Central <javalledor@outlook.com>
2021-02-01 16:32:00 +0100

    Program 05 ->Timeout & Interval

--- 05. Dates - Math/index-date-math.js
+++ 05. Dates - Math/index-date-math.js
@@ -24,3 +24,23 @@ console.log(`pow() function: Number 2 to the 3rd power is ${Math.pow(2, 3)}`);
 console.log(`random() function: Random number between 0 and almost 1 returned is ${Math.random()}`);
 console.log(`max() function: the max number of '3, 6, 9, 1' is ${Math.max(3, 6, 9, 1)}`);
 console.log(`min() function: the min number of '3, 6, 9, 1' is ${Math.min(3, 6, 9, 1)}`);
+
+
+// setTimeOut
+const aTimeOut1 = setTimeout(function () {
+    console.log('Timeout after 3 seconds')
+}, 3000);
+
+const aTimeOut2 = setTimeout(() => {
+    console.log('Timeout arrow after 4 seconds')
+}, 4000);
+
+
+// setInterval
+const anInterval1 = setInterval(function () {
+    console.log('Interval after 5 seconds')
+}, 5000);
+
+const anInterval2 = setInterval(() => {
+    console.log('Interval arrow after 10 seconds')
+}, 10000);
